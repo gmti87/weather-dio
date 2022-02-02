06b872120133fc7d963c41b0c0a133b2fc4d3341
gmti87 <gmoreti87@gmail.com>
2022-02-02 14:59:04 -0300

    Projeto weather dio

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { CityWeather } from 'src/app/shared/models/weather.model';
+
+import * as fromHomeActions from './state/home.actions';
+import * as fromHomeSelectors from './state/home.selectors';
+
+@Component({
+  selector: 'jv-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss']
+})
+export class HomePage implements OnInit {
+
+  cityWeather$: Observable<CityWeather>;
+  loading$: Observable<boolean>;
+  error$: Observable<boolean>;
+
+  searchControl: FormControl;
+
+  text: string;
+
+  constructor(private store: Store) { }
+
+  ngOnInit(): void {
+    this.searchControl = new FormControl('', Validators.required);
+
+    this.cityWeather$ = this.store.pipe(select(fromHomeSelectors.selectCurrentWeather));
+    this.loading$ = this.store.pipe(select(fromHomeSelectors.selectCurrentWeatherLoading));
+    this.error$ = this.store.pipe(select(fromHomeSelectors.selectCurrentWeatherError));
+  }
+
+  doSearch() {
+    const query = this.searchControl.value;
+    this.store.dispatch(fromHomeActions.loadCurrentWeather({ query }));
+  }
+
+}
